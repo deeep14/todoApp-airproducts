617f5359aea44cf496f5217a1b8f8acbf6f9261f
Deepak Masuti <deepak.s.masuti@intimetec.com>
2024-05-29 10:49:50 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'todoapp';
+  readonly APIUrl="http://localhost:5038/api/todoapp/";
+
+  constructor(private http:HttpClient){
+  }
+  notes:any=[];
+
+  refreshNotes(){
+    this.http.get(this.APIUrl+'GetNotes').subscribe(data=>{
+      this.notes=data;
+    })
+  }
+
+  ngOnInit(){
+    this.refreshNotes();
+  }
+
+  addNotes(){
+    var newNotes=(<HTMLInputElement>document.getElementById("newNotes")).value;
+    var formData=new FormData();
+    formData.append("newNotes",newNotes);
+    this.http.post(this.APIUrl+'AddNotes',formData).subscribe(data=>{
+      alert(data);
+      this.refreshNotes();
+    })
+  }
+
+
+   deleteNotes(id:any){
+    
+    this.http.delete(this.APIUrl+'DeleteNotes?id='+id).subscribe(data=>{
+      alert(data);
+      this.refreshNotes();
+    })
+  }
+}
