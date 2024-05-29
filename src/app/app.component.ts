359d67966fd36974e37a3d231952ed5ab4b0032c
Deepak Masuti <deepak.s.masuti@intimetec.com>
2024-05-29 12:20:59 +0530

    adding frontend code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,46 +1,47 @@
 import { Component } from '@angular/core';
-
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule
+import { CommonModule } from '@angular/common'; // Import CommonModule
 
 @Component({
+  standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports: [HttpClientModule, CommonModule] // Add CommonModule here
 })
 export class AppComponent {
   title = 'todoapp';
-  readonly APIUrl="http://localhost:5038/api/todoapp/";
+  readonly APIUrl="http://localhost:5256/api/todoapp/";
 
-  constructor(private http:HttpClient){
+  constructor(private http: HttpClient) {}
+
+  notes: any = [];
+
+  refreshNotes() {
+    this.http.get(this.APIUrl + 'GetNotes').subscribe(data => {
+      this.notes = data;
+    });
   }
-  notes:any=[];
 
-  refreshNotes(){
-    this.http.get(this.APIUrl+'GetNotes').subscribe(data=>{
-      this.notes=data;
-    })
-  }
-
-  ngOnInit(){
+  ngOnInit() {
     this.refreshNotes();
   }
 
-  addNotes(){
-    var newNotes=(<HTMLInputElement>document.getElementById("newNotes")).value;
-    var formData=new FormData();
-    formData.append("newNotes",newNotes);
-    this.http.post(this.APIUrl+'AddNotes',formData).subscribe(data=>{
+  addNotes() {
+    var newNotes = (<HTMLInputElement>document.getElementById("newNotes")).value;
+    var formData = new FormData();
+    formData.append("newNotes", newNotes);
+    this.http.post(this.APIUrl + 'AddNotes', formData).subscribe(data => {
       alert(data);
       this.refreshNotes();
-    })
+    });
   }
 
-
-   deleteNotes(id:any){
-    
-    this.http.delete(this.APIUrl+'DeleteNotes?id='+id).subscribe(data=>{
+  deleteNotes(id: any) {
+    this.http.delete(this.APIUrl + 'DeleteNotes?id=' + id).subscribe(data => {
       alert(data);
       this.refreshNotes();
-    })
+    });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,46 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule
+import { CommonModule } from '@angular/common'; // Import CommonModule
 
-import { AppComponent } from './app.component';
-import {HttpClientModule} from '@angular/common/http';
-
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  imports: [CommonModule] // Add CommonModule here
 })
-export class AppModule { }
+export class AppComponent {
+  title = 'todoapp';
+  readonly APIUrl="http://localhost:5038/api/todoapp/";
+
+  constructor(private http: HttpClient) {}
+
+  notes: any = [];
+
+  refreshNotes() {
+    this.http.get(this.APIUrl + 'GetNotes').subscribe(data => {
+      this.notes = data;
+    });
+  }
+
+  ngOnInit() {
+    this.refreshNotes();
+  }
+
+  addNotes() {
+    var newNotes = (<HTMLInputElement>document.getElementById("newNotes")).value;
+    var formData = new FormData();
+    formData.append("newNotes", newNotes);
+    this.http.post(this.APIUrl + 'AddNotes', formData).subscribe(data => {
+      alert(data);
+      this.refreshNotes();
+    });
+  }
+
+  deleteNotes(id: any) {
+    this.http.delete(this.APIUrl + 'DeleteNotes?id=' + id).subscribe(data => {
+      alert(data);
+      this.refreshNotes();
+    });
+  }
+}
